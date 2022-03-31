c090c28fb4361bc891df1b87dd7fedc8a66f91aa
Savage <savage@savagedev.io>
2022-03-31 17:38:05 -0400

    Rework how we define stubDirectory

--- app/SlimSkeleton/Console/Traits/Generatable.php
+++ app/SlimSkeleton/Console/Traits/Generatable.php
@@ -4,24 +4,29 @@ namespace SlimSkeleton\Console\Traits;
 
 trait Generatable
 {
-    protected $stubDirectory = __DIR__ . "/../../stubs";
+    public $stubDirectory = null;
+
+    private function getStubDirectory()
+    {
+        return $this->stubDirectory;
+    }
 
     public function generateStub($name, $replacements)
     {
         return str_replace(
             array_keys($replacements),
             $replacements,
-            file_get_contents($this->stubDirectory . "/" . $name . ".stub")
+            file_get_contents($this->getStubDirectory() . "/" . $name . ".stub")
         );
     }
 
     public function getAndMoveStub($path, $stubName, $fileName, $ext = ".php")
     {
-        if(isset($this->stubDirectory)) {
+        if($this->getStubDirectory()) {
             return false;
         }
 
-        $stub = file_get_contents($this->stubDirectory . "/" . $stubName . ".stub");
+        $stub = file_get_contents($this->getStubDirectory() . "/" . $stubName . ".stub");
 
         $stubTo = $path . "/" . $fileName . $ext;
 
