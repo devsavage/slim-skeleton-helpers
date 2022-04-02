68af57fac6b32b18b9a239c8fbf69ade942ef669
Savage <savage@savagedev.io>
2022-04-02 01:10:40 -0400

    Need to declare the rootDir

--- app/SlimSkeleton/Console/Console.php
+++ app/SlimSkeleton/Console/Console.php
@@ -8,24 +8,30 @@ use Symfony\Component\Console\Application;
 
 class Console extends Application
 {
-    protected $app;
+    protected $app, $rootDir;
 
-    public function __construct(App $app, $version)
+    public function __construct(App $app, $version, $rootDir)
     {
         parent::__construct("Slim Skeleton Base", $version);
 
         $this->app = $app;
+        $this->rootDir = $rootDir;
     }
 
     public function boot(KernelInterface $kernel) 
     {
         foreach($kernel->getCommands() as $command) {
-            $this->add(new $command($this->getApp()->getContainer()));
+            $this->add(new $command($this->getApp()->getContainer(), $this->getRootDir()));
         }
     }
 
     private function getApp()
     {
         return $this->app;
+    }
+
+    public function getRootDir()
+    {
+        return $this->rootDir;
     }
 }
